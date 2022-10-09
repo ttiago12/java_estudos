ddb185d4f1454dc09e909001a7672274779edb5d
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-10-09 15:39:37 -0300

    Inicio

---
+++ src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primeiroprograma;
+
+/**
+ *
+ * @author emaiu.
+ */
+
+public class PrimeiroPrograma{
+    public static void main (String[] args){
+//        sout - aperta a tecla tab
+        System.out.println("Escreve e pula linha");
+        
+        System.out.print("Ola , mundo");
+        System.out.print("Ok");
+        
+        
+    }
+}
