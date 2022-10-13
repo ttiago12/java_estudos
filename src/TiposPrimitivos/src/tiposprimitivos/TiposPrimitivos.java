343040c970bf8fd1a94fe5758107eb1e991b4d08
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-10-13 18:42:23 -0300

    up

--- src/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ src/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -4,6 +4,8 @@
  */
 package tiposprimitivos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author emaiu
@@ -15,15 +17,29 @@ public class TiposPrimitivos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        float nota = 8.5f;
-        String nome = "Gustavo";
-        float nota1 = (float) 8.5;
-        System.out.println("A nota e "+ nota);
+//        float nota = 8.5f;
+//        String nome = "Gustavo";
+//        float nota1 = (float) 8.5;
+//        System.out.println("A nota e "+ nota);
+//        
+//        System.out.println("A nota e "+ nota1);
+//        
+////        Print formatado , configurando a quantidade de cadas decimais
+//        System.out.printf ("A nota de e %s %.2f \n",nome, nota);
+////       mesma formatação
+//        System.out.format("A nota de e %s %.2f \n",nome, nota);
+//        
+//        leitura de dados
+        Scanner teclado = new Scanner(System.in);
         
-        System.out.println("A nota e "+ nota1);
+        System.out.println("Digite o nome");
+        String nomeT = teclado.nextLine(); 
+        System.out.println("Digite a nota");
+        float nota2 = teclado.nextFloat();
         
-//        Print formatado , configurando a quantidade de cadas decimais
-        System.out.printf ("A nota de %s %.2f \n",nome, nota);
+         System.out.format("A nota de e %s %.6f \n",nomeT, nota2);
+        
     }
+    
     
 }
