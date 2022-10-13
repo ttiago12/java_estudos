d5bf6bf778ba21f060e0b67c306f65197e4cf7d4
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-10-13 17:55:58 -0300

    rq

---
+++ src/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiposprimitivos;
+
+/**
+ *
+ * @author emaiu
+ */
+public class TiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float nota = 8.5f;
+        float nota1 = (float) 8.5;
+        System.out.print("A nota e "+ nota);
+        System.out.println("");
+        System.out.println("A nota e "+ nota1);
+                
+    }
+    
+}
